9ca2051003af1643cc6eae9f1df76292a3ca8348
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-16 17:44:36 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,6 +6,7 @@ ARCH=$(uname -m)
 
 echo "Building package and its dependencies..."
 echo "---------------------------------------------------------------"
+make-aur-package deno-stable-bin
 make-aur-package python-emoji-country-flag
 make-aur-package aria2p
 # Switch to the official package when it updates
