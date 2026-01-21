c3ecd4247328b6e483bb110e4d0da939075b780d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-21 14:32:43 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,13 +4,14 @@ set -eu
 
 ARCH=$(uname -m)
 
+pacman -Syu --noconfirm python-charset-normalizer jq
+
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 echo "Building package and its dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm python-charset-normalizer jq
 make-aur-package deno-stable-bin
 make-aur-package python-emoji-country-flag
 make-aur-package aria2p
